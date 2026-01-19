115457d7921b36cc16aabb95b1075157b9b5ac2c
LeR08 <94314176+LeR08@users.noreply.github.com>
2026-01-19 16:44:38 +0100

    Delete bmws19 directory

--- bmws19/vehicle_names.lua
+++
@@ -1,7 +0,0 @@
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
-Citizen.CreateThread(function()
-     AddTextEntry("bmws19", "BMW S1000 2019")
-end)
